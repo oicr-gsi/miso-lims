1f6f2848e50f9964ab674eec7dfb0fd8ee5c1e25
Kyle Verhoog <kyleverhoog@oicr.on.ca>
2015-09-15 13:50:09 -0400

    added flexibility to deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )
-curl --user "jenkins:deployer" --upload-file "${DIR}/ROOT.war" --url ${1}
+curl --user ${1} --upload-file "${DIR}/${2}" --url ${3}
 
