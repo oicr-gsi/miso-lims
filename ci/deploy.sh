f320f56f1f1cb02d22b91d6428b13d8abe174aa5
Kyle Verhoog <kyleverhoog@oicr.on.ca>
2015-09-15 14:11:13 -0400

    test again

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 DIR=$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )
-curl --user ${1} --upload-file ${2} --url ${3}
+curl --user ${1} --upload-file "${WORKSPACE}/${2}" --url ${3}
 
