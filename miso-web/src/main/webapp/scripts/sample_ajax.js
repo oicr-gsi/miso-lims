3235e021c52ab97fa7c628ae27c453052ed35e4b
Dillan Cooke <dillan.cooke@oicr.on.ca>
2015-10-14 17:12:35 -0400

    search by identificationBarcode in List Samples page

--- miso-web/src/main/webapp/scripts/sample_ajax.js
+++ miso-web/src/main/webapp/scripts/sample_ajax.js
@@ -622,8 +622,8 @@ Sample.ui = {
             { "sTitle": "Alias"},
             { "sTitle": "Type"},
             { "sTitle": "QC Passed"},
-            { "sTitle": "QC Result"}
-            //{ "sTitle": "Edit"}
+            { "sTitle": "QC Result"},
+            { "sTitle": "ID", "bVisible": false}
           ],
           "bJQueryUI": true,
           "iDisplayLength": 25,
