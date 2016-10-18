50806efdc09b57e7d3c7e033e521cc77a2cf62c7
Andre Masella <andre.masella@oicr.on.ca>
2016-10-18 13:03:11 -0400

    Fixup

--- sqlstore/src/main/resources/db/migration/V8800__neo_mr_kit.sql
+++ sqlstore/src/main/resources/db/migration/V8800__neo_mr_kit.sql
@@ -1,3 +1,3 @@
 INSERT INTO KitDescriptor(name, version, manufacturer, partNumber, stockLevel, kitType, platformType, description, lastModifier)
-VALUES ('Neo MR', 0, 'Illumina', 1, 0, 'Library', 'Illumina', 'n/a', 1);
+VALUES ('Neo MR', 0, 'Illumina', 1, 0, 'Library', 'Illumina', 'NeoPrep mRNA library preparation', 1);
 
