23e743fd9ac51a9a5edfc79d5dd8ce5789418d26
Heather Armstrong <heather.armstrong@oicr.on.ca>
2016-11-07 09:30:28 -0500

    fixup change lDC to lDCId

--- sqlstore/src/main/resources/db/migration/V8251__add_more_libraryDesigns.sql
+++ sqlstore/src/main/resources/db/migration/V8251__add_more_libraryDesigns.sql
@@ -3,7 +3,7 @@ INSERT INTO LibraryDesignCode (code, description) VALUES ("TR", "TR");
 
 DELETE FROM LibraryDesign WHERE sampleClassId = (SELECT sampleClassId FROM SampleClass WHERE alias = "cDNA (aliquot)") AND name = "SM";
 
-INSERT INTO LibraryDesign (name, librarySelectionType, libraryStrategyType, libraryDesignCode, sampleClassId) VALUES
+INSERT INTO LibraryDesign (name, librarySelectionType, libraryStrategyType, libraryDesignCodeId, sampleClassId) VALUES
   ("WG", (SELECT librarySelectionTypeId FROM LibrarySelectionType WHERE name = "PCR"), (SELECT libraryStrategyTypeId FROM LibraryStrategyType WHERE name = "WGS"), (SELECT libraryDesignCodeId FROM LibraryDesignCode WHERE code = "WG"), (SELECT sampleClassId FROM SampleClass WHERE alias = "gDNA_wga (aliquot)")),
   ("TS (Hybrid Selection)", (SELECT librarySelectionTypeId FROM LibrarySelectionType WHERE name = "Hybrid Selection"), (SELECT libraryStrategyTypeId FROM LibraryStrategyType WHERE name = "AMPLICON"), (SELECT libraryDesignCodeId FROM LibraryDesignCode WHERE code = "TS"), (SELECT sampleClassId FROM SampleClass WHERE alias = "gDNA_wga (aliquot)")),
   ("TS (PCR)", (SELECT librarySelectionTypeId FROM LibrarySelectionType WHERE name = "PCR"), (SELECT libraryStrategyTypeId FROM LibraryStrategyType WHERE name = "AMPLICON"), (SELECT libraryDesignCodeId FROM LibraryDesignCode WHERE code = "TS"), (SELECT sampleClassId FROM SampleClass WHERE alias = "gDNA_wga (aliquot)")),
