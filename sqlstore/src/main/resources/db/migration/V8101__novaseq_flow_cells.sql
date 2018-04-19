18af00636feed914400a984500dd4b64bdc61d05
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-04-19 12:07:08 -0400

    GLT-2273: Change alias of S2/S4 flow cells to NovaSeq {S2,S4} Flow Cell

---
+++ sqlstore/src/main/resources/db/migration/V8101__novaseq_flow_cells.sql
@@ -0,0 +1,8 @@
+-- novaseq_flow_cells
+
+-- StartNoTest
+UPDATE SequencingContainerModel SET alias='NovaSeq S2 Flow Cell' WHERE alias='S2 Flow Cell';
+UPDATE SequencingContainerModel SET alias='NovaSeq S4 Flow Cell' WHERE alias='S4 Flow Cell';
+-- EndNoTest
+
+
